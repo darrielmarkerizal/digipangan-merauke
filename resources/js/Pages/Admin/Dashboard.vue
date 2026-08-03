b76373f60c4912499356791eaf016c6b5ca9eb0c
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-08-03 09:58:23 +0700

    style: fix dashboard layout to place lists side-by-side at the bottom

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -52,17 +52,17 @@ const props = defineProps<{
         <div class="mt-4 grid grid-cols-1 gap-5 lg:grid-cols-12">
             <div class="lg:col-span-8 flex flex-col gap-5">
                 <DashboardTrendChart :trend-data="props.trend_data" />
-
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
-                    <DashboardPopularProducts
-                        :popular-products="props.popular_products"
-                    />
-                </div>
             </div>
 
             <div class="lg:col-span-4 flex flex-col gap-5">
                 <DashboardRegionDistribution
                     :region-distribution="props.region_distribution"
+                />
+            </div>
+
+            <div class="lg:col-span-12 grid grid-cols-1 md:grid-cols-2 gap-5">
+                <DashboardPopularProducts
+                    :popular-products="props.popular_products"
                 />
                 <DashboardRecentActivities
                     :recent-activities="props.recent_activities"
